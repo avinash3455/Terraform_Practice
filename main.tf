2af0fc2b73c6bff31eca04a113ff016d76e3a8ae
Avinash <avinash@gmail.>
2025-10-16 23:26:30 +0530

    changes done

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = "t2.micro"
 }
+
